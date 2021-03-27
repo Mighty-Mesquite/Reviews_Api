661d48aabef584fd80972cedc8f63d5633f0c19b
Ubuntu <ubuntu@ip-172-31-5-211.us-west-1.compute.internal>
2021-03-27 04:39:11 +0000

    reconfigured connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  user: 'root',
+const connection = mysql.createPool({
+  host: 'ec2-54-183-213-207.us-west-1.compute.amazonaws.com',
+  port: 3306,
+  user: 'root4',
   password: 'password',
-  database: 'reviewsapi'
+  database: 'reviewsapi',
+  waitForConnection: true,
+  connectionLimit: 1000,
+  connectionTimeout: 60*60*1000,
+  queueLimit: 0
 })
 
-connection.connect((err) => {
+connection.getConnection((err) => {
   if(err) {
     console.log(err);
   } else {
